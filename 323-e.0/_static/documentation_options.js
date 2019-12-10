81e2aee2e3b5e6b327086753bb87a873995769a6
Manfred Moser <manfred@starburstdata.com>
2019-12-10 09:53:39 -0800

    Fix version to 323-e.0

--- 323-e.0/_static/documentation_options.js
+++ 323-e.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '323-e.1-SNAPSHOT',
+    VERSION: '323-e.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
